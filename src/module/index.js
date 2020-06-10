c108b2ba5f273bd9d54e0e6e7f55f73dae93e8f6
shivam03yadav <shivam.yadav@successive.tech>
2020-06-10 17:05:02 +0530

    feat: add changes

--- src/module/index.js
+++ src/module/index.js
@@ -1,14 +1,10 @@
 import { fileLoader, mergeTypes } from "merge-graphql-schemas";
 import path from 'path';
-
 import query from './user/index';
 import * as trainee from './trainee/index';
 
 const typeArray = fileLoader(path.join(__dirname, './**/*.graphql'));
 const typeDefs = mergeTypes(typeArray, { all:true });
-
-console.log('user query is ', query)
-console.log('trainee query is ', trainee.Query);
 
 export default{
   resolvers: {
