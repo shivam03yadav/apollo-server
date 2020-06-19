287d8b8eaa7617fdc57048c4556717a5b94e973d
shivam03yadav <shivam.yadav@successive.tech>
2020-06-19 19:57:44 +0530

    feat: add changes

--- src/module/trainee/query.js
+++ src/module/trainee/query.js
@@ -3,6 +3,6 @@ export default{
     const { options: { skip, limit },} = args;
     const { dataSources: { traineeAPI }, } = context;
     const response = await traineeAPI.getTrainee({ skip, limit });
-    return response.data.records;
+    return response.data;
   },
 };
